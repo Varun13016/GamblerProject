37078f408bf269bd0c4fd1f7bcd2f90600f183b3
“Varun13016” <dmevarun@gmail.com>
2021-12-02 19:28:48 +0530

    UC2

---
+++ Gamblerproject/GamblerWinOrLoss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamblerproject
+{
+    public class GamblerWinOrLoss
+    {
+        public static void WinOrLoss()
+        {
+            int Start = 100;
+
+            Random random = new Random();
+
+            int n = random.Next(2);
+
+            if (n == 1)
+            {
+                Start++;
+
+                Console.WriteLine("Gambler Win the $1 bet");
+                Console.WriteLine("Your current Stack is : " + Start);
+            }
+            else
+            {
+                Start--;
+                Console.WriteLine("Gambler Loss the $1 bet");
+                Console.WriteLine("Your current Stack is : " + Start);
+            }
+
+        }
+    }
+}
--- Gamblerproject/Program.cs
+++ Gamblerproject/Program.cs
@@ -6,12 +6,11 @@ namespace Gamblerproject
     {
         static void Main(string[] args)
         {
-            double cash, betAmount;
-            Console.WriteLine("Welcome to Gambler Game");
-            Console.WriteLine("Please Enter Cash Amount");
-            cash = double.Parse(Console.ReadLine());
-            Console.WriteLine("Please Enter Bet Amount");
-            betAmount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Welcom Gambling Stimulation Progarm \n");
+
+            GamblerWinOrLoss obj = new GamblerWinOrLoss();
+
+            obj.WinOrLoss();
         }
     }
 }
