b478049079c8787a58a85813becaafb130508087
“Varun13016” <dmevarun@gmail.com>
2021-12-02 19:12:42 +0530

    UC1

---
+++ Gamblerproject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gamblerproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cash, betAmount;
+            Console.WriteLine("Welcome to Gambler Game");
+            Console.WriteLine("Please Enter Cash Amount");
+            cash = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Bet Amount");
+            betAmount = double.Parse(Console.ReadLine());
+        }
+    }
+}
